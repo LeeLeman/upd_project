60076e89ccd67e1121b7788b06718d601934b6ce
Andrei Kirianov <andrei.kirianov@emergingtravel.com>
2024-11-10 10:17:00 +0100

    default system admin

--- database.py
+++ database.py
@@ -1,11 +1,30 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
+
+from common import UserRole
 
 DATABASE_URL = "sqlite:///./sqlite.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+def create_admin_user(db: Session):
+    from accounts.models import User
+    from accounts.logic import hash_password
+    admin_exists = db.query(User).filter(User.role == UserRole.SYSTEM_ADMIN).first()
+    if not admin_exists:
+        admin_user = User(
+            first_name="Admin",
+            last_name="Admin",
+            email="admin@test.ru",
+            contact_number="+71234567899",
+            password=hash_password("admin123"),
+            role=UserRole.SYSTEM_ADMIN
+        )
+        db.add(admin_user)
+        db.commit()
 
 
 def init_db():
@@ -16,6 +35,8 @@ def init_db():
     import models
 
     Base.metadata.create_all(bind=engine)
+    with SessionLocal() as session:
+        create_admin_user(session)
 
 
 def get_db():
